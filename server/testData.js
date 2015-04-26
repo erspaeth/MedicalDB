bc58a9186aa2419777ba8748d526873dee86398d
Craig Hogarth <achogarth@gmail.com>
2015-04-26 15:48:25 -0500

    patients added

--- server/testData.js
+++ server/testData.js
@@ -1,9 +1,76 @@
 /*
   Add test data to the collections
 */
-
+Meteor.startup(function () {
 
 // Patients
+if (true){
+  var patient1 = {
+    firstName: 'Gytha',
+    lastName: 'Ogg',
+    dateOfBirth: '05/29/1915',
+    patientNum: '0000',
+    gender: 'F',
+    height: '60',
+    weight: '250',
+    conditions: ['Addiction (Alcohol Abuse)'],
+    assistiveDevices: [],
+    primaryProvider: '',
+    visits: []
+  };
+
+  Patients.insert(patient1);
+
+  var patient2 = {
+    firstName: 'Samuel',
+    lastName: 'Vimes',
+    dateOfBirth: '08/13/1949',
+    patientNum: '0001',
+    gender: 'M',
+    height: '65',
+    weight: '200',
+    conditions: ['Scoliosis'],
+    assistiveDevices: ['Walking Cane'],
+    primaryProvider: '',
+    visits: []
+  };
+
+  Patients.insert(patient2);
+
+  var patient3 = {
+    firstName: 'Mustrum',
+    lastName: 'Ridcully',
+    dateOfBirth: '02/20/2004',
+    patientNum: '0002',
+    gender: 'M',
+    height: '45',
+    weight: '95',
+    conditions: ['Epilepsy'],
+    assistiveDevices: [''],
+    primaryProvider: '',
+    visits: []
+  };
+
+  Patients.insert(patient3);
+
+  var patient4 = {
+    firstName: 'Terry',
+    lastName: 'Pratchett',
+    dateOfBirth: '04/28/1948',
+    patientNum: '0003',
+    gender: 'M',
+    height: '67',
+    weight: '170',
+    conditions: ['Alzheimers'],
+    assistiveDevices: [''],
+    primaryProvider: '',
+    visits: []
+  };
+
+  Patients.insert(patient4);
+}
+
+});
 
 
 //Visits
@@ -30,6 +97,6 @@
     // the second {} says what update we are making, use $push to add the visitID
     // to the array of all visit._id's
 
-    Patients.update({_id: patient._id}, {$push: {visits: myVisitID}})
+    Patients.update({_id: patient._id}, {$push: {visits: myVisitID}});
 
 */
